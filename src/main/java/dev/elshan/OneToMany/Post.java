5de0264a962dc6467e2eab0ba6e24242bf1c1e66
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2024-06-24 09:45:12 +0400

    Added one to many

--- src/main/java/dev/elshan/OneToMany/Post.java
+++ src/main/java/dev/elshan/OneToMany/Post.java
@@ -1,16 +1,16 @@
-//package dev.elshan.OneToMany;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//public class Post {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String content;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private SocialUser socialUser;
-//}
+package dev.elshan.OneToMany;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private SocialUser socialUser;
+}
--- src/main/java/dev/elshan/OneToMany/SocialUser.java
+++ src/main/java/dev/elshan/OneToMany/SocialUser.java
@@ -1,18 +1,18 @@
-//package dev.elshan.OneToMany;
-//
-//import jakarta.persistence.*;
-//
-//import java.util.List;
-//
-//@Entity
-//public class SocialUser {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String name;
-//    private String email;
-//
-//    @OneToMany(mappedBy = "socialUser")
-//    private List<Post> posts;
-//}
+package dev.elshan.OneToMany;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class SocialUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts;
+}
